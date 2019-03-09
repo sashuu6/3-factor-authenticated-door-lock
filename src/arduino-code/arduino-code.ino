89d6022ffd22e86c3e5d4f2907cd74f190dda45f
Sashwat K <sashwat0001@gmail.com>
2019-03-09 23:46:50 +0530

    working on LCD

--- src/arduino-code/arduino-code.ino
+++ src/arduino-code/arduino-code.ino
@@ -13,6 +13,27 @@
 // Revision: 1
 // Last Edited: 09 Mar 2019
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#include <Keypad.h>
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 7, 8}; //connect to the column pinouts of the keypad
+
+char key;
+int temp,passCode;
+
 void setup() {
   
 }
