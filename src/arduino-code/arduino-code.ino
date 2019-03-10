dcc03d1c6b45dc861ed2fdc08615f9b1e89028d0
Sashwat K <sashwat0001@gmail.com>
2019-03-10 09:57:26 +0530

    new changes

--- src/arduino-code/arduino-code.ino
+++ src/arduino-code/arduino-code.ino
@@ -5,7 +5,7 @@
 //        \$$$$$$\   $$$$$$$ |\$$$$$$\  $$ |  $$ |$$ |  $$ |$$ |  $$ |$$  __$$\ 
 //         \____$$\ $$  __$$ | \____$$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ /  $$ |
 //        $$$$$$$  |\$$$$$$$ |$$$$$$$  |$$ |  $$ |\$$$$$$  |\$$$$$$  | $$$$$$  |
-//        \_______/  \_______|\_______/ \__|  \__| \______/  \______/  \______/ 
+//        \_______/  \_______|\_______/ \__|  \__| \______/  \______/  \______/
 //
 // Project name: 3-factor authenticated lock
 // Author: Sashwat K
@@ -23,21 +23,40 @@ LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {6, 7, 8}; //connect to the column pinouts of the keypad
 
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 char key;
-int temp,passCode;
+int temp;
+long passCode = 0;
 
 void setup() {
-  
+  Serial.begin(9600);
+  lcd.begin(16, 2);
 }
 
 void loop() {
-  
+  key = keypad.getKey();
+  if (key) {
+    if (key == '#') {
+      passCode = 0;
+      lcd.clear();
+      Serial.print("passCode cleared");
+    }
+    else {
+      temp = key - '0';
+      passCode = (passCode * 10) + temp;
+      Serial.println(passCode);
+      lcd.clear();
+      lcd.print("*");
+      delay(1500);
+    }
+  }
 }
