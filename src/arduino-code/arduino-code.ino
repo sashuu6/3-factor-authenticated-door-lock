03782018d96cf137a81926171c2e84e57d676794
Sashwat K <sashwat0001@gmail.com>
2019-03-10 12:06:51 +0530

    arduino code complete

--- src/arduino-code/arduino-code.ino
+++ src/arduino-code/arduino-code.ino
@@ -33,35 +33,73 @@ byte colPins[COLS] = {6, 7, 8}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
+int firstAuthentication = 10;
+int secondAuthentication = 11;
+
 char key;
 int temp,n;
 long passCode = 0;
+long correctPassCode = 8454842;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
+  pinMode(firstAuthentication, INPUT);
+  lcd.clear();
 }
 
 void loop() {
-  int i=0;
-  key = keypad.getKey();
-  if (key) {
-    if (key == '#') {
-      passCode = 0;
-      lcd.clear();
-      n=0;
-      Serial.print("passCode cleared");
-    }
-    else {
-      temp = key - '0';
-      passCode = (passCode * 10) + temp;
-      n++;
-      lcd.clear();
-      while(i<n) {
-        lcd.print("*");
-        i++;
+  int firstState = digitalRead(firstAuthentication);
+  if (firstState == 1) {
+    int i=0;
+    key = keypad.getKey();
+    if (key) {
+      if (key == '#') {
+        passCode = 0;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("cleared");
+        n=0;
+        delay(500);
       }
-      delay(1500);
+      else if(key == '*') {
+        if(passCode == correctPassCode) {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Access granted");
+          digitalWrite(secondAuthentication, HIGH);
+          Serial.println("DONE");
+          delay(500);
+        }
+        else {
+          lcd.clear();
+          lcd.setCursor(0, 0);
+          lcd.print("Access denied");
+          delay(500);
+        }
+      }
+      else {
+        temp = key - '0';
+        passCode = (passCode * 10) + temp;
+        n++;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Enter passcode: ");
+        lcd.setCursor(0, 1);
+        while(i<n) {
+          lcd.print("*");
+          i++;
+        }
+        delay(500);
+      }
     }
+  }
+  else {
+    lcd.setCursor(0, 0);
+    lcd.print("Key switch");
+    lcd.setCursor(0, 1);
+    lcd.print("OFF");
+    delay(500);
+    lcd.clear();
   }
 }
