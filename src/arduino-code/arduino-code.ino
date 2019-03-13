f07ad76ff888bf1a45c61c6e74b50e20cfd6b0a0
Sashwat K <sashwat0001@gmail.com>
2019-03-14 00:01:23 +0530

    arduino code complete

--- src/arduino-code/arduino-code.ino
+++ src/arduino-code/arduino-code.ino
@@ -18,6 +18,12 @@
 
 #include <Keypad.h>
 
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 const byte ROWS = 4; //four rows
@@ -28,66 +34,148 @@ char keys[ROWS][COLS] = {
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
+
 byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {6, 7, 8}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-int firstAuthentication = 10;
-int secondAuthentication = 11;
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-char key;
+MFRC522::MIFARE_Key key; 
+
+byte myIFID[4] = {145,14,128,185};
+
+int firstAuthentication = A1;
+int thirdAuthentication = A2;
+
+char valKey;
 int temp,n;
 long passCode = 0;
 long correctPassCode = 8454842;
+int state1 = 0;
+int state2 = 0;
+int state3 = 0;
+boolean state1_state;
+boolean state2_state;
 
 void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+ 
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
   pinMode(firstAuthentication, INPUT);
-  pinMode(secondAuthentication, OUTPUT);
+  pinMode(thirdAuthentication, OUTPUT);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Vsh's 3 - Factor");
+  lcd.setCursor(0, 1);
+  lcd.print("Authetication");
+  delay(2000);
   lcd.clear();
 }
 
 void loop() {
-  int firstState = digitalRead(firstAuthentication);
-  Serial.println("--------------------------");
-  Serial.print("Key switch: ");Serial.println(firstState);
-  if (firstState == 1) {
+  state1 = rfid_find();
+  Serial.println(state1);
+  if (state1 == 1) {
+    if (state1_state == 0) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("NFC tag");
+      lcd.setCursor(0, 1);
+      lcd.print("Recognised");
+      state1_state = 1;
+      delay(1000);
+      lcd.clear();
+    }
+    state2 = keypad_authentication();
+    Serial.println(state2);
+    if (state2 == 1) {
+      analogWrite(thirdAuthentication,100);
+      Serial.println("Authentication complete");
+    }
+  }
+  else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Place");
+    lcd.setCursor(0, 1);
+    lcd.print("NFC tag");
+    delay(500);
+  }
+}
+
+boolean rfid_find() {
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial())
+    return;
+    
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  
+  if (rfid.uid.uidByte[0] == myIFID[0] || 
+    rfid.uid.uidByte[1] == myIFID[1] || 
+    rfid.uid.uidByte[2] == myIFID[2] || 
+    rfid.uid.uidByte[3] == myIFID[3] ) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+boolean keypad_authentication() {
+  int firstState = analogRead(firstAuthentication);
+  if (state2_state == 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Enter key");
+    delay(1000);
+    state2_state = 1;
+  }
+  if (firstState >= 500) {
     int i=0;
-    key = keypad.getKey();
-    if (key) {
-      if (key == '#') {
+    valKey = keypad.getKey();
+    if (valKey) {
+      if (valKey == '#') {
         passCode = 0;
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("cleared");
-        Serial.println("Pin Cleared");
-        digitalWrite(secondAuthentication, LOW);
+        digitalWrite(thirdAuthentication, LOW);
         n=0;
         delay(500);
         lcd.clear();
+        state2_state = 0;
       }
-      else if(key == '*') {
+      else if(valKey == '*') {
         if(passCode == correctPassCode) {
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Access granted");
-          digitalWrite(secondAuthentication, HIGH);
-          Serial.println("Access granted");
-          delay(500);
+          lcd.setCursor(0, 1);
+          return 1;
         }
         else {
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Access denied");
-          Serial.println("Access denied");
-          digitalWrite(secondAuthentication, LOW);
+          digitalWrite(thirdAuthentication, LOW);
           delay(500);
+          state2_state = 0;
+          return 0;
         }
       }
       else {
-        temp = key - '0';
+        temp = valKey - '0';
         passCode = (passCode * 10) + temp;
         n++;
         lcd.clear();
@@ -103,11 +191,15 @@ void loop() {
     }
   }
   else {
+    passCode = 0;
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Key switch");
     lcd.setCursor(0, 1);
     lcd.print("OFF");
     delay(500);
     lcd.clear();
+    state2_state = 0;
   }
+  delay(10);
 }
--- src/nodeMCU-code/nodeMCU-code.ino
+++ src/nodeMCU-code/nodeMCU-code.ino
@@ -13,44 +13,18 @@
 // Revision: 1
 // Last Edited: 10 Mar 2019
 
-#include <SPI.h>
-#include <MFRC522.h>
-
-constexpr uint8_t RST_PIN = D5;     // Configurable, see typical pin layout above
-constexpr uint8_t SS_PIN = D4;     // Configurable, see typical pin layout above
-
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
-
-MFRC522::MIFARE_Key key; 
-
-byte myIFID[4] = {145,14,128,185};
 int switchKey = D0;
 int firstAuthentication = D3;
 int thirdAuthentication = D4;
 
 void setup() {
   Serial.begin(115200);
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
   pinMode(switchKey, INPUT);
   pinMode(firstAuthentication, OUTPUT);
   pinMode(thirdAuthentication, INPUT);
-  
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
 }
 
 void loop() {
-  // Look for new cards
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
- 
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   int keySwitchState = digitalRead(switchKey);
   int thirdAuthenticationState = digitalRead(thirdAuthentication);
   if(keySwitchState == 1) {
@@ -59,14 +33,6 @@ void loop() {
     Serial.println(keySwitchState);
     if(thirdAuthenticationState == 1) {
       Serial.println("2nd authentication complete");
-      // Look for new cards
-
-      if (rfid.uid.uidByte[0] == myIFID[0] || 
-        rfid.uid.uidByte[1] == myIFID[1] || 
-        rfid.uid.uidByte[2] == myIFID[2] || 
-        rfid.uid.uidByte[3] == myIFID[3] ) {
-          Serial.println(F("Yooo"));
-        }
     }
     else {
       Serial.println("2nd authentication incomplete");
@@ -75,9 +41,5 @@ void loop() {
   else {
     digitalWrite(firstAuthentication, LOW);
   }
-  // Halt PICC
-  rfid.PICC_HaltA();
- 
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+  
 }
